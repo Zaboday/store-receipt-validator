e9db417e2b98c0042f7316a6f0cb091380fd648b
v.abzalov <v.abzalov@gmail.com>
2019-11-24 13:25:51 +0500

    Added validationModePurchase parameter to Google ServiceAccountValidator

--- src/GooglePlay/ServiceAccountValidator.php
+++ src/GooglePlay/ServiceAccountValidator.php
@@ -13,17 +13,21 @@ class ServiceAccountValidator
      * ServiceAccountValidator constructor.
      *
      * @param array $options
+     * @param bool  $validationModePurchase
      *
      * @throws \Google_Exception
      */
-    public function __construct($options = [])
+    public function __construct($options = [], $validationModePurchase = false)
     {
         $googleClient = new \Google_Client();
         $googleClient->setScopes([\Google_Service_AndroidPublisher::ANDROIDPUBLISHER]);
         $googleClient->setApplicationName($options['application_name']);
         $googleClient->setAuthConfig($options['path_to_service_account_json']);
 
-        $this->validator = new \ReceiptValidator\GooglePlay\Validator(new \Google_Service_AndroidPublisher($googleClient));
+        $this->validator = new \ReceiptValidator\GooglePlay\Validator(
+            new \Google_Service_AndroidPublisher($googleClient),
+            $validationModePurchase
+        );
     }
 
     /**
